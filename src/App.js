7ac74f4db92cdf7e429320c45d6fe167789750da
FernandoAdrogue <chinoadrogue@gmail.com>
2023-11-03 19:32:25 -0300

    add display result

--- src/App.js
+++ src/App.js
@@ -1,16 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {AZURE_COGNITIVE_SERVICES_KEY, AZURE_COGNITIVE_SERVICES_ENDPOINT} from 'process.env';
 
 function App() {
-  const [inputValue, setInputValue] = React.useState("");
-  //const [cursorPosition, setCursorPosition] = React.useState(0);
+  const [inputValue, setInputValue] = useState('');
+  const [result, setResult] = useState(null);
 
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
   };
 
-  // const handleInputFocus = (event) => {
-  //   setCursorPosition(event.target.selectionStart);
-  // };
+  const analyzeImage = async () => {
+    const subscriptionKey = AZURE_COGNITIVE_SERVICES_KEY;
+    const endpoint = AZURE_COGNITIVE_SERVICES_ENDPOINT;
+    const uriBase = `${endpoint}/computervision/imageanalysis:analyze&api-version=2023-02-01-preview`;
+
+    const params = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Ocp-Apim-Subscription-Key': subscriptionKey,
+      },
+      body: JSON.stringify({ url: inputValue }),
+    };
+
+    const response = await fetch(uriBase, params);
+    const data = await response.json();
+    setResult(data);
+  };
+
+  const DisplayResults = () => {
+    if (result) {
+      return (
+        <div>
+          <h2>Results:</h2>
+          <p>Image URL: {inputValue}</p>
+          <p>Image format: {result.metadata.format}</p>
+          <p>Image width: {result.metadata.width}</p>
+          <p>Image height: {result.metadata.height}</p>
+          <p>Image description: {result.description.captions[0].text}</p>
+        </div>
+      );
+    } else {
+      return null;
+    }
+  };
 
   return (
     <div>
@@ -25,13 +58,15 @@ function App() {
           placeholder="Enter URL or textual prompt to generate an image"
           value={inputValue}
           onChange={handleInputChange}
-          //onFocus={handleInputFocus}
         />
         <div>
-          <button type="button">Analice</button>
+          <button type="button" onClick={analyzeImage}>
+            Analice
+          </button>
           <button type="button">Generate</button>
         </div>
       </form>
+      <DisplayResults />
     </div>
   );
 }
